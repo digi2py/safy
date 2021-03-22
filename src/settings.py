714f75963ec494dcd1893ac3d5780780b70edd97
Digi2py <alaouitaib@my.smccd.edu>
2021-03-22 20:29:48 +0100

    Prepare for Heroku with Procfile and Runtime

--- src/settings.py
+++ src/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'k&st+ao4uthjrfc7n(veq8j2#6jz697h(q2w4h!-8!g*oxfo4a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition

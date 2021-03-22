c0b373adbc4abb95cdde5e7d764648107bee9d84
Digi2py <alaouitaib@my.smccd.edu>
2021-03-22 23:20:25 +0100

    Ready For Heroku

--- src/settings.py
+++ src/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'k&st+ao4uthjrfc7n(veq8j2#6jz697h(q2w4h!-8!g*oxfo4a'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['safy-blog.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
